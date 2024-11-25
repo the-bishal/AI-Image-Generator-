cb25666d104dae7135cf0e32b424f2d55a3ea2b2
Bishal Gupta <guptabishal111@gmail.com>
2024-11-25 12:58:14 +0530

    Responsiveness added and fixed minor bugs

--- src/components/ImageGenerator.jsx
+++ src/components/ImageGenerator.jsx
@@ -21,7 +21,11 @@ const ImageGenerator = () => {
 
     useEffect(()=>{
         if(imgUrl){
-            setLoading(false);
+
+            setTimeout(()=>{
+                setLoading(false);
+            },15000)
+
         }
     },[imgUrl]);
 
@@ -37,43 +41,50 @@ const ImageGenerator = () => {
     style={{backgroundImage: `url(${background})`}}
     >
         <div className='flex flex-col m-auto items-center mt-[2.5rem] gap-8'>
-        <h2 className='text-7xl font-semibold pb-8'>
-            AI Image <span className='text-pink-700 font-bold'>Generator</span>
-        </h2>
+            <h2 className='text-[10vw] font-semibold pb-8 md:text-7xl '>
+                AI Image <span className='text-pink-700 font-bold'>Generator</span>
+            </h2>
 
-        <div className='Card'>
+            <div className='Card w-4/5 flex flex-col items-center justify-center'>
 
-            <div className='ImageDiv flex flex-col justify-center items-center w-[60rem]'>
-                <img 
-                    src={inputRef.current ? imgUrl : defaultImg} 
-                    className='w-[48rem] h-[30rem]'
-                />
-            </div>
-            <div className="loading">
-                <div 
-                    className="loadingBar my-[0.2rem] mx-auto rounded-lg w-[48rem] h-[0.5rem] bg-pink-700 relative -left-5 duration-[15s]"
-                    style={loading ? { width: "48rem"}:{width:"0"}}
-                >
+                <div className='ImageDiv flex flex-col justify-center items-center w-full md:w-4/5 lg:w-3/4 max-w-[720px] overflow-auto'>
+                    <img 
+                        src={inputRef.current ? imgUrl : defaultImg} 
+                        className='w-full'
+                    />
                 </div>
-                {loading && (<div 
-                    className="text-xl relative left-24 font-bold "
-                >Loading....</div>)}
-            </div>
+                <div className="loading w-full">
+                    <div 
+                        className="loadingBar text-left my-[0.2rem] mx-auto rounded-lg w-full md:w-4/5 lg:w-3/4 max-w-[720px] h-[0.5rem] bg-pink-700"
+                        style={loading ? {transitionDuration:"15s", width: "48rem"}:{transitionDuration:"0s",width:"0"}}
+                    >
+                    </div>
+                    {loading && (<div 
+                        className="text-xl text-pink-700 relative left-24 font-bold lg:ml-24"
+                    >Loading....</div>)}
+                </div>
 
-            <div className='flex w-[62.5rem] h-24 justify-around items-center rounded-full bg-white mt-8'>
-                <input 
-                    type="text" 
-                    ref={inputRef} 
-                    placeholder="Describe Your Image"
-                    className='w-[37.5rem] h-12 bg-transparent border-none outline-none text-xl font-bold text-pink-900 pl-8 mr-8 placeholder:text-[#999] placeholder:font-normal'
-                     />
+                <div className='flex w-full h-12 md:h-16 px-2 lg:h-24 justify-around items-center rounded-full bg-white mt-8'>
+                    <input 
+                        type="text" 
+                        ref={inputRef} 
+                        placeholder="Describe Your Image"
+                        className='w-4/5 h-8 bg-transparent border-none outline-none text-xl font-bold text-pink-900 pl-8 mr-8 placeholder:text-[#999] placeholder:font-normal'
+                        />
+                    <button
+                        onClick={handleClick}
+                        className='genBtn items-center justify-center px-8 py-6 text-white text-3xl rounded-full bg-pink-600 cursor-pointer hidden lg:flex'
+                        > Generate 
+                    </button>
+                </div>
+
                 <button
-                    onClick={handleClick}
-                    className='genBtn flex items-center justify-center -mr-4 w-[18.75rem] h-[5.5rem] text-white text-3xl rounded-full bg-pink-600 cursor-pointer'
-                > Generate </button>
-            </div>
+                        onClick={handleClick}
+                        className='genBtn mt-8 md:mt-4 items-center justify-center px-8 py-2 md:py-4 text-white text-3xl rounded-full bg-pink-600 cursor-pointer flex lg:hidden'
+                        > Generate 
+                </button>
 
-        </div>
+            </div>
         </div>
     </section>
   )
